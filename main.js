729302dd4c74a0b8476530d9082765e066e09cf7
yummyX02 <1245191297@qq.com>
2022-07-12 14:09:52 +0800

    finish the development of home

--- main.js
+++ main.js
@@ -2,6 +2,34 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+// 导入第三方网络请求包
+import { $http } from '@escook/request-miniprogram'
+
+// 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
+uni.$http = $http
+//配置api
+$http.baseUrl = "https://api-hmugo-web.itheima.net"
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+
+//封装showMsg()方法
+uni.$showNsg = function (title = '数据加载失败',duration = 1500){
+  uni.showToast({
+    title,
+    duration,
+    icon:"none"
+  })
+}
+
 
 Vue.config.productionTip = false
 
